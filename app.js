ce03321587cce04310824b07ce6331d501153692
tenhold <tenhold@gmail.com>
2021-02-04 16:00:53 -0600

    (add) upto 62

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const http = require('http');
-const { handlerUser, handler } = require('./routes');
 
-const server = http.createServer(handler);
+const express = require('express');
 
-server.listen(3000, () => {
-  console.log(`listen on localhost:3000`);
+// const { handler } = require('./routes');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('in the middleware');
+  next(); // allows the request to continue to the next middleware in the line
+});
+
+app.use((req, res, next) => {
+  console.log('in another middleware');
+  res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('app listening on port http://localhost:3000');
 });
