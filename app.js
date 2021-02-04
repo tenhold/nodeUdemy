daea519071a01ba7b52e28dff04594df0ec96280
tenhold <tenhold@gmail.com>
2021-02-04 15:41:09 -0600

    (add) section4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const { handlerUser, handler } = require('./routes');
 
-const server = http.createServer(handlerUser);
+const server = http.createServer(handler);
 
 server.listen(3000, () => {
   console.log(`listen on localhost:3000`);
