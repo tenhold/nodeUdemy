7d7d21adb4d57926a096cc362a415f8d1958d9de
tenhold <tenhold@gmail.com>
2021-02-04 16:16:17 -0600

    (add) 63

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const http = require('http');
 
-const express = require('express');
+const app = require('express')();
 
-// const { handler } = require('./routes');
-
-const app = express();
-
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next(); // allows the request to continue to the next middleware in the line
+app.use('/user', (req, res, next) => {
+  console.log('hello from /user');
+  res.send('<h1>hello from "/user"</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log('in another middleware');
-  res.send('<h1>Hello from express</h1>');
+app.use('/', (req, res, next) => {
+  console.log('hello from /');
+  res.send('<h1>hello from "/"</h1>');
 });
+
 
 app.listen(3000, () => {
-  console.log('app listening on port http://localhost:3000');
+  console.log('listen on http://localhost:3000');
 });
