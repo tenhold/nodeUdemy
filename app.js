3c2e01c198477e4ca626747fda86b7e81987df1e
tenhold <tenhold@gmail.com>
2021-02-08 15:00:48 -0600

    (add) static files to express using css up to 75

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const path = require('path');
+const express = require('express');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = require('express')();
+const app = express();
 
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
