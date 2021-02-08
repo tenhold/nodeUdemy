1caa76a8cacb54fcc5b73a6c09c15ff798294c43
tenhold <tenhold@gmail.com>
2021-02-08 14:49:52 -0600

    (add) styling to html

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = require('express')();
+
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000, () => {
